f0ec48b122fefe86703a23435972512915cc6233
Luiz Fernando <luizfernando.nb@outlook.com>
2021-11-17 12:38:25 -0300

    Ajustes nas páginas

---
+++ js/tecnologia.js
@@ -0,0 +1,10 @@
+const ir_topo = document.querySelector("#ir_topo");
+
+ir_topo.addEventListener("click", function () {
+window.scrollTo({
+	//rodar a pagina até:
+		top:0,
+		left:0,
+		behavior:"smooth"
+    })        
+});
