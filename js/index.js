75450ce1288b7e6d2d647abcb8b819c3778d6bbc
Luiz Fernando <luizfernando.nb@outlook.com>
2021-11-18 19:56:57 -0300

    Ajustes finais

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let economia = document.getElementById('artigo-noticia-economia'),
+    esportes = document.getElementById('artigo-noticia-esportes'),
+	tecnologia = document.getElementById('artigo-noticia-tecnologia'),
+	lazer = document.getElementById('artigo-noticia-lazer'),
+	podcasts = document.getElementById('artigo-noticia-podcast'),
+	politica = document.getElementById('artigo-noticia-politica');
+	
+economia.onclick = function () {
+	window.location.href = "../html/economia.html";
+};
+
+esportes.onclick = function () {
+	window.location.href = "../html/esportes.html";
+};
+
+tecnologia.onclick = function () {
+	window.location.href = "../html/tecnologia.html";
+};
+
+lazer.onclick = function () {
+	window.location.href = "../html/lazer.html";
+};
+
+podcasts.onclick = function () {
+	window.location.href = "../html/podcast.html";
+};
+
+politica.onclick = function () {
+	window.location.href = "../html/politica.html";
+};
